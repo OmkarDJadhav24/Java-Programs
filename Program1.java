b6b4cc43a2bcfe33fad084f1cf7bcca50eaf4fc0
OmkarDJadhav24 <101233431+OmkarDJadhav24@users.noreply.github.com>
2022-07-10 12:27:41 +0530

    Add files via upload

--- Program1.java
+++ Program1.java
@@ -1,37 +1,21 @@
 import java.io.*;
 import java.util.*;
 
+
 class Program1
 {
     public static void main(String arg[])
     {
-        int i=0,j=0;
         Scanner sobj = new Scanner(System.in);
         
-        System.out.println("Enter number of rows");
-        int irow = sobj.nextInt();
+        System.out.println("Enter the string : ");
+        String str = sobj.nextLine();
         
-        System.out.println("Enter number of column");
-        int icol = sobj.nextInt();
-        
-        int Arr[][] = new int[irow][icol];
-        
-        System.out.println("Enter the elements: ");
-        for(i=0;i<Arr.length;i++)
+        char arr[] = str.toCharArray();
+        for(int i=0;i<arr.length;i++)
         {
-            for(j=0;j<Arr[i].length;j++)
-            {
-                Arr[i][j] = sobj.nextInt();
-            }
-        }
-        
-        for(i=0;i<Arr.length;i++)
-        {
-            for(j=0;j<Arr[i].length;j++)
-            {
-                System.out.print(Arr[i][j]+" ");
-            }
-            System.out.println();
+            System.out.println(arr[i]);
         }
     }
+
 }
--- Program5.java
+++ Program5.java
@@ -1,42 +1,22 @@
 import java.io.*;
 import java.util.*;
 
+
 class Program5
 {
     public static void main(String arg[])
     {
-        int i=0,j=0;
-        int iSum = 0;
-        
         Scanner sobj = new Scanner(System.in);
         
-        System.out.println("Enter number of rows");
-        int irow = sobj.nextInt();
+        System.out.println("Enter the string : ");
+        String str = sobj.nextLine();
         
-        System.out.println("Enter number of column");
-        int icol = sobj.nextInt();
+        String newstr = str.replaceAll("\\s+"," ");
+        String newstr2 = newstr.trim();
         
-        int Arr[][] = new int[irow][icol];
+        String arr[] = newstr2.split(" ");
         
-        System.out.println("Enter the elements: ");
-        for(i=0;i<Arr.length;i++)
-        {
-            for(j=0;j<Arr[i].length;j++)
-            {
-                Arr[i][j] = sobj.nextInt();
-            }
-        }
-        
-        for(i=0;i<Arr.length;i++)
-        {
-            for(j=0;j<Arr[i].length;j++)
-            {
-                iSum = iSum + Arr[i][j]; 
-            }
-            System.out.println("Addition is: "+iSum);
-            iSum = 0;
-            
-        }
-        
+        System.out.println("Number of words are : "+arr.length);
     }
+
 }
